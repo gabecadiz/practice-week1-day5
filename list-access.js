86a817c59f1e4716cce90f4d312140c63c44ba07
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-02-08 18:00:07 +0000

    creating and exporting modules exercise

--- list-access.js
+++ list-access.js
@@ -1,5 +1,5 @@
 var listAccess = require("./list-builder");
-// console.log(listAccess);
+// console.log(listAccess);//
 listAccess.listFiller(5);
 listAccess.listFiller(30);
 listAccess.listFiller(78);
--- list-builder.js
+++ list-builder.js
@@ -13,5 +13,5 @@ module.exports = {
   },
   listOutput: function() {
     console.log(listSorter());
-  }
+   }
 };
