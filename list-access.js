8598151e6316c539f58a449f62c08b03988c9961
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-02-08 17:57:13 +0000

    creating and exporting modules function

---
+++ list-access.js
@@ -0,0 +1,8 @@
+var listAccess = require("./list-builder");
+// console.log(listAccess);
+listAccess.listFiller(5);
+listAccess.listFiller(30);
+listAccess.listFiller(78);
+listAccess.listFiller(23);
+listAccess.listFiller(13);
+listAccess.listOutput();
---
+++ list-builder.js
@@ -0,0 +1,17 @@
+var researchList = [];
+
+var listSorter = function() {
+  return researchList.sort(function(a,b) {
+    return a - b;
+  });
+};
+
+module.exports = {
+  listFiller: function(num) {
+    researchList.push(num);
+    // console.log(researchList);
+  },
+  listOutput: function() {
+    console.log(listSorter());
+  }
+};
